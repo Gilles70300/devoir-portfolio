baaed44dc66e99bb1292d3eda887f162ae3c2033
Gilles Winand <gilles.winand@gmail.com>
2024-09-05 17:22:44 +0200

    Travail sur la modale

--- src/components/ModalePage.vue
+++ src/components/ModalePage.vue
@@ -1,11 +1,41 @@
 <template>
-    <div class="bloc-modale" v-if="revele">
+    <div class="bloc-modale" v-if="revele1">
 
-        <div class="overlay" v-on:click="toggleModale"></div>
+        <div class="overlay" v-on:click="toggleModale1"></div>
 
         <div class="modale card">
-            <div class="btn-modale btn btn-danger" v-on:click="toggleModale">X</div>
-            <h2>{{ creationTitle }}</h2>
+            <div class="btn-modale btn btn-danger" v-on:click="toggleModale1">X</div>
+            <h2>Curriculum Vitae</h2>
+            <p>Date de création: 26 juin 2024</p>
+            <p>Technologies utilisées: HTML5 et CSS3 (Flexbox)</p>
+            <a href="https://github.com/Gilles70300/Projet-devoir-CV" target="blank">Lien vers le GitHub du projet</a>
+        </div>
+
+    </div>
+
+    <div class="bloc-modale" v-if="revele2">
+
+        <div class="overlay" v-on:click="toggleModale2"></div>
+
+        <div class="modale card">
+            <div class="btn-modale btn btn-danger" v-on:click="toggleModale2">X</div>
+            <h2>Cahier des charges</h2>
+            <p>Date de création: 08 juillet 2024</p>
+            <a href="../assets/creations/cahier des charges/Cahier des charges La Socketterie.pdf" download target="blank">Pour voir le cahier des charges, cliquez ici!</a>
+        </div>
+
+    </div>
+
+    <div class="bloc-modale" v-if="revele3">
+
+        <div class="overlay" v-on:click="toggleModale3"></div>
+
+        <div class="modale card">
+            <div class="btn-modale btn btn-danger" v-on:click="toggleModale3">X</div>
+            <h2>Dynamisme d'un espace commentaire</h2>
+            <p>Date de création: 31 juillet 2024</p>
+            <p>Technologies utilisées: HTML5 et Javascript</p>
+            <a href="https://github.com/Gilles70300/DynamiserUnEspaceDeCommentaires_DocumentsDevoir" target="_blank">Lien vers le GitHub du projet</a>
         </div>
 
     </div>
@@ -15,8 +45,12 @@
 export default {
     name: "ModalePage",
     props: [
-        'revele', 
-        'toggleModale', 
-        'creationTitle']
+        'revele1', 
+        'revele2',
+        'revele3',
+        'toggleModale1', 
+        'toggleModale2',
+        'toggleModale3'
+    ]
 }
 </script>
--- src/components/PortfolioPage.vue
+++ src/components/PortfolioPage.vue
@@ -5,34 +5,25 @@
         <div>
             <h2>Mon curriculum vitae</h2>
             <img src="../assets/creations/cv/projet-cv.jpg" alt="Photo de mon projet de CV">
-            <modalePage 
-            creation-title="CV" 
-            v-bind:revele="revele" 
-            v-bind:toggleModale="toggleModale"/>
+            <modalePage v-bind:revele1="revele1" v-bind:toggleModale1="toggleModale1"/>
 
-            <div class="btn btn-success" v-on:click="toggleModale">Plus d'infos sur le projet</div>
+            <div class="btn btn-success" v-on:click="toggleModale1">Plus d'infos sur le projet</div>
         </div>
     
         <div>
             <h2>Cahier des charges</h2>
             <img src="../assets/creations/cahier des charges/cahier des charges.jpg" alt="Exemple d'un projet de cahier des charges pour un site web">
-            <modalePage 
-            creation-title="Cahier des charges" 
-            v-bind:revele="revele" 
-            v-bind:toggleModale="toggleModale" />
+            <modalePage v-bind:revele2="revele2" v-bind:toggleModale2="toggleModale2" />
 
-            <div class="btn btn-success" v-on:click="toggleModale">Plus d'infos sur le projet</div>
+            <div class="btn btn-success" v-on:click="toggleModale2">Plus d'infos sur le projet</div>
         </div>
     
         <div>
             <h2>Dynamisme d'un espace commentaire</h2>
             <img src="../assets/creations/espace commentaire/Dynamisme espace commentaire.jpg" alt="Photo de mon projet de dynamisme d'un espace commentaire">
-            <modalePage 
-            creation-title="Dynamisme d'un espace commentaire" 
-            v-bind:revele="revele" 
-            v-bind:toggleModale="toggleModale" />
+            <modalePage v-bind:revele3="revele3" v-bind:toggleModale3="toggleModale3" />
 
-            <div class="btn btn-success" v-on:click="toggleModale">Plus d'infos sur le projet</div>
+            <div class="btn btn-success" v-on:click="toggleModale3">Plus d'infos sur le projet</div>
         </div>
     </div>
 </template>
@@ -43,15 +34,23 @@
         name: "PortfolioPage",
         data() {
             return {
-                revele: false
+                revele1: false,
+                revele2: false,
+                revele3: false
             }
         },
         components: {
             'modalePage': ModalePage
         },
         methods: {
-            toggleModale: function (){
-                this.revele = !this.revele
+            toggleModale1: function (){
+                this.revele1 = !this.revele1
+            },
+            toggleModale2: function (){
+                this.revele2 = !this.revele2
+            },
+            toggleModale3: function (){
+                this.revele3 = !this.revele3
             }
         }
     }
